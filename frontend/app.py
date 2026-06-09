7e4c90bf1388fef11530d3b17f31d2bef38168d1
brahmani26m-oss <brahmani.26.m@gmail.com>
2026-06-09 21:46:30 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -4,6 +4,10 @@ import os
 # Add project root to sys.path for submodule imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+
+
+
+
 import streamlit as st
 from PIL import Image
 import time
